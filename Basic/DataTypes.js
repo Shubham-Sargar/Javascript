72a6bcec1c7400c043c247ec8cd82618774318ba
Shubham Sargar <sargarshubham69@gmail.com>
2026-08-07 22:02:41 +0530

    datatypes

---
+++ Basic/DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js jc code as newer version
+
+//alert(3 + 3) // ew are using the node js not browser
+
+console.log(3+3); 
+
+//code readability that shoukd high.
+
+/**
+ *   Datatype :-
+ * 
+ * number 
+ * bigint 
+ * string
+ * boolean
+ * null :- standalone value
+ * undefined :-  valus not assigned.
+ * symbol 
+ * objects
+ *
+ */
+console.log(typeof(null))
+console.log( typeof(nudefined))
