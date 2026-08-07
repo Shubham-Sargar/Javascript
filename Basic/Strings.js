ba0275a2504e5da946b220e2f10271453590fffa
Shubham Sargar <sargarshubham69@gmail.com>
2026-08-07 23:14:19 +0530

    String operation

---
+++ Basic/Strings.js
@@ -0,0 +1,17 @@
+const name= "shubham";
+const surname ="sargar"
+const repoCount =50
+console.log(name +" "+surname);
+/**
+ * String operation
+ * 
+ * charAt
+ * indexOf
+ * substring
+ * slice
+ * trim  :-> remove  extra space
+ * replace
+ */
+ const url=" https://google.com";
+ url.replace('oo', '-');
+ 
