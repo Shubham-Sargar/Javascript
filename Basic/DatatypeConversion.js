d7b2ecbac16f32adaa3c63aa08e0a9e73be29171
Shubham Sargar <sargarshubham69@gmail.com>
2026-08-07 22:14:43 +0530

    type conversion

---
+++ Basic/DatatypeConversion.js
@@ -0,0 +1,16 @@
+let score  =undefined ;
+console.log(typeof score);
+
+let valueInNumber = Number (score)
+console.log(typeof(valueInNumber));
+/**
+ * 
+ * "33" => 33
+ * true = 1 false = 0
+ * 
+ * 
+ */
+
+let isLoggedIn =" hitesh"
+let booleanIsloggedIn = Boolean(isLoggedIn);
+console.log(booleanIsloggedIn);
