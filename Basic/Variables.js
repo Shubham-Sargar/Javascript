7d966d6d1e0feb1df50d5c5b23fdf7f849f28d07
Shubham Sargar <sargarshubham69@gmail.com>
2026-08-07 21:49:54 +0530

    Variables in javascript

---
+++ Basic/Variables.js
@@ -0,0 +1,17 @@
+const accountId =  144553; // const = lack the value not changeble
+let acountEmail ="abc@gmail.com";
+var accountPassword =" 12345"
+accounCity ="Jaipur"
+
+//accountId =2  not allowed this thype of use 
+
+accountEmail ="xyz@gamil.com"
+accountpassword = " 121212"
+accountCity = " bengluru"
+
+let accountState ;
+/***
+ * Prefersd to use the let and const not to use the var
+ */
+console.log(accountId)
+console.log(accountState)
